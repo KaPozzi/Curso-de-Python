abf6ec01b518c108e5eff32c50ca875abeabfa94
Kauã Pozzi <110647374+KaPozzi@users.noreply.github.com>
2022-08-09 21:51:30 -0300

    Add files via upload

---
+++ Desafio1.py
@@ -0,0 +1,7 @@
+#Desafio 1
+
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+
+print('Olá', nome, 'Muito Prazer,você tem', idade, 'e seu peso é', peso)
---
+++ Desafio3.py
@@ -0,0 +1,6 @@
+# Desafio 3
+
+numero = input('Primeiro Número')
+numero2 = input('Segundo Número')
+soma = int(numero) + int(numero2)
+print('A soma é', soma) 
